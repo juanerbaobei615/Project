653fcf90976b41fc15ce2db0415d9510a4cd9588
ghostmb <337924752@qq.com>
2019-03-19 15:09:23 +0800

    yunqidujia

---
+++ yunqi/js/lib.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function($) {	
+	$('.ico-menu').click(function(e){            
+        $('.m-nav').stop().slideToggle(400);
+        e.stopPropagation();
+    });
+});
+
+
+$(document).ready(function($) {
+    $(".TAB li").click(function(){
+      var tab=$(this).parent(".TAB");
+      var con=tab.attr("id");
+      var on=tab.find("li").index(this);
+      $(this).addClass('on').siblings(tab.find("li")).removeClass('on');
+      $(con).eq(on).show().siblings(con).hide();
+    });
+});
+
+
+
